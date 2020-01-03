8e8faee6d5bfe95a3c3cd56c5692a902d85ffbd2
Akash Das <akashdas.ny@gmail.com>
2020-01-02 21:23:43 -0500

    write to file

--- control.h
+++ control.h
@@ -10,13 +10,13 @@
 #include <unistd.h>
 
 
-union semun {
-               int              val;    /* Value for SETVAL */
-               struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-               unsigned short  *array;  /* Array for GETALL, SETALL */
-               struct seminfo  *__buf;  /* Buffer for IPC_INFO
-                                           (Linux-specific) */
-           };
+// union semun {
+//   int              val;    /* Value for SETVAL */
+//   struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+//   unsigned short  *array;  /* Array for GETALL, SETALL */
+//   struct seminfo  *__buf;  /* Buffer for IPC_INFO
+//                               (Linux-specific) */
+// };
 
 union semun arg;
 
--- write.c
+++ write.c
@@ -1,9 +1,32 @@
+#include "control.h"
+
 int main(){
+  char * data;
+  sb.sem_num = 0;
+  sb.sem_op = -1;
+  file = open("story.txt", O_WRONLY | O_APPEND);
+
+  semid = semget(SEMKEY, 1, 0);
   printf("trying to get in\n");
-  printf("Last addition:\n");
-  char nextline[100];
+  semop(semid, &sb, 1);
+
+
+  shmid = shmget(SHKEY, 1000, IPC_CREAT | 0644);
+  data = shmat(shmid, 0, 0);
+
+  printf("Last addition: %s\n", data);
+
   printf("Your addition: ");
-  scanf("%s", nextline);
-  printf("Your addition: %s\n", nextline);
+  fgets(data, 1000, stdin);
+
+  write(file, data, strlen(data));
+  close(file);
+
+  //printf("Your addition: %s\n", nextline);
+
+  shmdt(data);
+
+  sb.sem_op = 1;
+  semop(semid, &sb, 1);
   return 0;
 }
